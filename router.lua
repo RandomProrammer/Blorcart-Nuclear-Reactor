6465a9ce1a28c8fa7d98c0b26da622441c9d3b03
Luke Carter Harding <LukeHarding@Harding-Imac-1.local>
2020-10-11 14:57:21 -0400

    dev init

---
+++ router.lua
@@ -0,0 +1,16 @@
+--API(s)
+local component = require("component")
+local event = require("event")
+local serialization = require("serialization")
+local io = require("io")
+--Component(s)
+local modem = component.modem
+--Function(s)
+local function MsgRec(componentId, receiverAddress, senderAddress, port, distance, message)
+    
+end
+--Listener(s)
+local MainLisener = event.listen("modem_message",MsgRec)
+--Exit
+event.pull("key_down")
+event.cancel(MainLisener)
