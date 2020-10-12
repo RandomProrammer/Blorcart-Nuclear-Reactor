1b7545d17d0ccf01920d3ccf5c1f9c3694ac7df7
luke110206 <lukeharding90@gmail.com>
2020-10-12 10:05:48 -0400

    10/12

--- router.lua
+++ router.lua
@@ -19,7 +19,11 @@ local RoutingTable = serialization.unserialize(SerilizeData:read())
 SerilizeData:close()
 --Function(s)
 local function FindAddrs(ToFind)
-
+    for _,v in pairs(RoutingTable) do
+        if v[1] == ToFind then
+            return v[2]
+        end
+    end
 end
 local function MsgRec(componentId, receiverAddress, senderAddress, port, distance, message)
     local MsgData = serialization.unserialize(message)
@@ -29,24 +33,40 @@ local function MsgRec(componentId, receiverAddress, senderAddress, port, distanc
         modem.send(FwrdAddr, Port, message)
     elseif MsgData["Type"] == "cmd" then
         local Sender = MsgData["Sender"]
-        local FwrdAddr = FindAddrs(Sender)
+        local To = MsgData["To"]
+        local FwrdAddr = FindAddrs(To)
+        local ResendAddr = FindAddrs(Sender)
         local Response = {}
-        Response["Type"] = "res"
-        Response["Sender"] = "1"
-        Response["To"] = Sender
-        Response["Data"] = "Instructions Not Supported"
-        local SerialzedResponse = serialization.serialize(Response)
-        modem.send(FwrdAddr, Port, SerialzedResponse)
+        if To == "1" then
+            Response["Type"] = "res"
+            Response["Sender"] = "1"
+            Response["To"] = Sender
+            Response["Data"] = "Instructions Not Supported"
+            local SerialzedResponse = serialization.serialize(Response)
+            modem.send(ResendAddr, Port, SerialzedResponse)
+        else
+            Response = message
+            local SerialzedResponse = serialization.serialize(Response)
+            modem.send(FwrdAddr, Port, SerialzedResponse)
+        end
     elseif MsgData["Type"] == "req" then
         local Sender = MsgData["Sender"]
-        local FwrdAddr = FindAddrs(Sender)
+        local To = MsgData["To"]
+        local FwrdAddr = FindAddrs(To)
+        local ResendAddr = FindAddrs(Sender)
         local Response = {}
-        Response["Type"] = "res"
-        Response["Sender"] = "1"
-        Response["To"] = Sender
-        Response["Data"] = RoutingTable
-        local SerialzedResponse = serialization.serialize(Response)
-        modem.send(FwrdAddr, Port, SerialzedResponse)
+        if To == "1" then
+            Response["Type"] = "res"
+            Response["Sender"] = "1"
+            Response["To"] = Sender
+            Response["Data"] = RoutingTable
+            local SerialzedResponse = serialization.serialize(Response)
+            modem.send(ResendAddr, Port, SerialzedResponse)
+        else
+            Response = message
+            local SerialzedResponse = serialization.serialize(Response)
+            modem.send(FwrdAddr, Port, SerialzedResponse)
+        end
     end
 end
 --Listener(s)
